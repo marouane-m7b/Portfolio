729b9f17a57d34ea3be6ffb68ce7424a3d15fb32
marouane_m7b <marouane.ma7boub@gmail.com>
2024-04-12 14:38:55 +0000

    Edit Skills part

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -14,6 +14,9 @@ import ProgressProvider from "./ProgressProvider";
 const Skills = () => {
   const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([]);
+  const [filterSkills, setFilterSkills] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("Front");
+  const [animateCard, setAnimateCard] = useState({ x: 0, opacity: 1 });
 
   useEffect(() => {
     const query = '*[_type == "experiences"]';
@@ -25,6 +28,7 @@ const Skills = () => {
 
     client.fetch(skillsQuery).then((data) => {
       setSkills(data);
+      setFilterSkills(data);
     });
   }, []);
 
@@ -69,50 +73,88 @@ const Skills = () => {
     );
   };
 
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ x: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ x: 0, opacity: 1 }]);
+
+      if (item === "All") {
+        setFilterSkills(skills);
+      } else {
+        setFilterSkills(skills.filter((skill) => skill.tags.includes(item)));
+      }
+    }, 500);
+  };
+
   return (
     <>
       <h2 className="head-text">Skills & Experiences</h2>
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills
-            .sort((a, b) => b.progress - a.progress)
-            .map((skill) => (
-              <motion.div
-                whileInView={{ opacity: [0, 1] }}
-                transition={{ duration: 0.5 }}
-                className="app__skills-item app__flex"
-                key={skill.name}
+          <div className="app__work-filter app__work-filter-skills">
+            {[
+              { show: "Frontend", send: "Front" },
+              { show: "Backend", send: "Back" },
+              { show: "Mobile", send: "Mobile" },
+              { show: "Other Tools", send: "Tools" }
+            ].map((item, index) => (
+              <div
+                key={index}
+                onClick={() => handleWorkFilter(item.send)}
+                className={`app__work-filter-item app__flex p-text ${
+                  activeFilter === item.send ? "item-active" : ""
+                }`}
               >
-                <div
-                  className="app__flex"
-                  style={{ backgroundColor: "transparent" }}
-                >
-                  <ProgressProvider valueStart={0} valueEnd={skill.progress}>
-                    {(value) => (
-                      <CircularProgressbarWithChildren
-                        value={value}
-                        styles={buildStyles({
-                          pathTransitionDuration: 0.5,
-                          pathColor: `rgba(62, 152, 199, ${value})`,
-                          trailColor: "#e3e3e3",
-                          backgroundColor: "#3e98c7",
-                        })}
-                        strokeWidth={3}
-                      >
-                        <img
-                          src={urlFor(skill.icon)}
-                          alt={skill.name}
-                          style={{ width: "50%", height: "50%" }} // Adjust image size to make it smaller
-                        />
-                        <span className="bold-text">{skill.progress}%</span>
-                      </CircularProgressbarWithChildren>
-                    )}
-                  </ProgressProvider>
-                </div>
-                <p className="p-text">{skill.name}</p>
-              </motion.div>
+                {item.show}
+              </div>
             ))}
+          </div>
+          <motion.div
+            className="app__work-portfolio"
+            animate={animateCard}
+            transition={{ duration: 0.5, delayChildren: 0.5 }}
+          >
+            {filterSkills
+              .sort((a, b) => b.progress - a.progress)
+              .map((skill) => (
+                <motion.div
+                  whileInView={{ opacity: [0, 1] }}
+                  transition={{ duration: 0.5 }}
+                  className="app__skills-item app__flex"
+                  key={skill.name}
+                >
+                  <div
+                    className="app__flex"
+                    style={{ backgroundColor: "transparent" }}
+                  >
+                    <ProgressProvider valueStart={0} valueEnd={skill.progress}>
+                      {(value) => (
+                        <CircularProgressbarWithChildren
+                          value={value}
+                          styles={buildStyles({
+                            pathTransitionDuration: 0.5,
+                            pathColor: `rgba(62, 152, 199, ${value})`,
+                            trailColor: "#e3e3e3",
+                            backgroundColor: "#3e98c7",
+                          })}
+                          strokeWidth={3}
+                        >
+                          <img
+                            src={urlFor(skill.icon)}
+                            alt={skill.name}
+                            style={{ width: "50%", height: "50%" }}
+                          />
+                        </CircularProgressbarWithChildren>
+                      )}
+                    </ProgressProvider>
+                  </div>
+                  <p className="p-text">{skill.name}</p>
+                </motion.div>
+              ))}
+          </motion.div>
         </motion.div>
         <div className="app__skills-exp">
           {experiences.map((experience) => (
