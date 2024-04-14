65f7e6d6633c6dc4b11ae438b6a40dd1ac105d4c
marouane_m7b <marouane.ma7boub@gmail.com>
2024-04-14 13:01:52 +0100

    Add logo + icons

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,13 +1,41 @@
 import React from "react";
 import "./App.scss";
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
-import { Header, Footer, About, Work, Skills, Testimonial, Experience } from "./container";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import {
+  Header,
+  Footer,
+  About,
+  Work,
+  Skills,
+  Testimonial,
+  Experience,
+} from "./container";
 import { Navbar } from "./components";
-import {SocialMediaFixed} from "./components";
+import { SocialMediaFixed } from "./components";
+import { Helmet } from "react-helmet";
+
 function App() {
   return (
     <div className="app">
+      <Helmet>
+        <title>Marouane Mahboub - Fullstack and Mobile Developer</title>
+        <meta
+          name="description"
+          content="Portfolio website of Marouane Mahboub, a fullstack and mobile developer. Explore projects, skills, and contact information."
+        />
+        <meta
+          name="keywords"
+          content="Marouane Mahboub, Marwane Mahboub, m7b, fullstack, m7b, fullstack developer, mobile developer, portfolio"
+        />
+        <script type="application/ld+json">
+          {`{
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Marouane Mahboub",
+            "url": "https://www.m7b.dev"
+          }`}
+        </script>
+      </Helmet>
       <Navbar />
       <Header />
       <About />
@@ -16,8 +44,8 @@ function App() {
       <Experience />
       <Testimonial />
       <Footer />
-      <SocialMediaFixed/>
-      <SpeedInsights/>
+      <SocialMediaFixed />
+      <SpeedInsights />
     </div>
   );
 }
